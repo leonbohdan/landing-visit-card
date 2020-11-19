f7c0771090cbd7ba72e2114e924b58f0431d5bb8
Bohdan Leonets <leonetsbogdan@gmail.com>
2020-11-19 22:06:00 +0200

    copy to clipboard was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const copy = document.getElementById('copy');
+const code = document.getElementById('code');
+
+const copyToClipboard = () => {
+  code.select();
+  code.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+};
+
+copy.addEventListener('click', () => {
+  copyToClipboard();
+});
